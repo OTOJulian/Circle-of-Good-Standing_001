e56c331a1d223631726c9313646b508295bff978
julian@one-to-one.nyc <info@julianisaac.com>
2026-01-19 22:12:33 -0600

    Simplify app - remove routing entirely, just render CirclePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
 import { CirclePage } from './pages/CirclePage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/circle/:token" element={<CirclePage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <CirclePage />;
 }
 
 export default App;
